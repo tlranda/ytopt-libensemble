da719963a43e0c449bedf00b17c5b9c2dcef997a
Tom Randall <tlranda@clemson.edu>
2023-07-25 16:06:23 +0000

    Add recollection job scripts

---
+++ ytopt-libe-heffte/polaris/combine/job_scripts/recollect_64_theta.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -x
+#COBALT -t 09:00:00
+#COBALT -n 513
+#COBALT --attrs filesystems=home,grand,eagle
+#COBALT -A EE-ECP
+#COBALT -q default
+
+source /home/trandall/theta_knl_heffte_env.sh;
+cd /home/trandall/ytune_23/tlranda-ytopt-libensemble/ytopt-libe-heffte/polaris/combine;
+
+app_scales=( 64 );
+mpi_ranks=( 4096 8192 );
+#mpi_ranks=( 64 128 256 512 1024 4096 8192 );
+#  nodes =    1   2   4   8   16   64  128
+workers=( 4 );
+# MAX nodes = 128 * 4 + 1 = 513 (12% cluster capacity)
+# THETA has MAX 4392 on DEFAULT queue
+calls=0;
+for app_scale in ${app_scales[@]}; do
+for n_ranks in ${mpi_ranks[@]}; do
+for n_workers in ${workers[@]}; do
+    echo "Calling on ${n_workers} workers with ${n_ranks} mpi ranks per worker for size ${app_scale}";
+    call="python libEwrapper.py --mpi-ranks ${n_ranks} --worker-timeout 300 --application-scale ${app_scale} --cpu-override 256 --cpu-ranks-per-node 64 --ensemble-workers ${n_workers} --max-evals 200 --configure-environment craympi --machine-identifier theta-knl --system theta --ens-dir-path Theta_${n_ranks}r_${app_scale}a --launch-job --display-results";
+    date;
+    echo "${call}";
+    eval "${call}";
+    date;
+    calls=$(( ${calls} + 1 ));
+done;
+echo;
+done;
+echo;
+done;
+echo "Requested ${calls} calls";
+
