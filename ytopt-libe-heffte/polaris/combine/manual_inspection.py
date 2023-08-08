ce9b07735116a3c4c263c1a0a4c2b339f2cd169e
Tom Randall <tlranda@clemson.edu>
2023-08-08 21:34:14 +0000

    Some bugfixes, but noting some instability which probably means there are more bugs to fixing

--- ytopt-libe-heffte/polaris/combine/manual_inspection.py
+++ ytopt-libe-heffte/polaris/combine/manual_inspection.py
@@ -33,8 +33,13 @@ truth = pd.read_csv('logs/ThetaSourceTasks/Theta_001n_0064a/manager_results.csv'
 truth['FLOPS'] *= -1
 truth = truth[truth['FLOPS'] > 0]
 truth = truth.sort_values(by=['FLOPS',]).reset_index(drop=True)
+print(f"Loaded {len(truth)} values for YTOPT true distribution")
 # Filter to what we'd like to see things look like
-best = truth[truth['FLOPS'] >= truth['FLOPS'].quantile(0.8)]
+quantile = 0.8
+best = truth[truth['FLOPS'] >= truth['FLOPS'].quantile(quantile)]
+print(f"Selected {len(best)} values for YTOPT best distribution (quantile = {quantile})")
+
+
 # Determine available values ish
 value_dict = dict((k, sorted(set(truth[k]))) for k in convert_cols)
 candidate_orders = [_ for _ in itertools.product([0,1,2], repeat=3) if len(_) == len(set(_))]
@@ -56,19 +61,18 @@ for col in ['p9']:
 source_files = [
 'logs/ThetaSourceTasks/Theta_002n_0064a/manager_results.csv',
 'logs/ThetaSourceTasks/Theta_004n_0064a/manager_results.csv',
-'logs/ThetaSourceTasks/Theta_008n_0064a/manager_results.csv',
-'logs/ThetaSourceTasks/Theta_016n_0064a/manager_results.csv',
-'logs/ThetaSourceTasks/Theta_032n_0064a/manager_results.csv',
-'logs/ThetaSourceTasks/Theta_064n_0064a/manager_results.csv',
-'logs/ThetaSourceTasks/Theta_128n_0064a/manager_results.csv',
+#'logs/ThetaSourceTasks/Theta_008n_0064a/manager_results.csv',
+#'logs/ThetaSourceTasks/Theta_016n_0064a/manager_results.csv',
+#'logs/ThetaSourceTasks/Theta_032n_0064a/manager_results.csv',
+#'logs/ThetaSourceTasks/Theta_064n_0064a/manager_results.csv',
+#'logs/ThetaSourceTasks/Theta_128n_0064a/manager_results.csv',
 ]
 sources = [pd.read_csv(_) for _ in source_files]
 for source in sources:
     source['FLOPS'] *= -1
     source = source.sort_values(by=['FLOPS',]).reset_index(drop=True)
-    # Filter as we would normally
-    source = source[source['FLOPS'] >= source['FLOPS'].quantile(0.8)]
 source_dist = pd.concat(sources).reset_index(drop=True)
+print(f"Loaded {len(source_dist)} values for TL distribution")
 
 # Set up TL model
 conditions = [Condition({'mpi_ranks': 64,
@@ -111,7 +115,8 @@ constraints = [{'constraint_class': 'ScalarRange', # App scale limit
                     },
               ]
 train_data = source_dist[['c0',]+compare_cols+['mpi_ranks', 'FLOPS']]
-train_data = train_data[train_data['FLOPS'] > train_data['FLOPS'].quantile(0.2)].drop(columns=['FLOPS'])
+train_data = train_data[train_data['FLOPS'] > train_data['FLOPS'].quantile(quantile)].drop(columns=['FLOPS'])
+print(f"Selected {len(train_data)} values for TL Training distribution (quantile = {quantile})")
 metadata = SingleTableMetadata()
 metadata.detect_from_dataframe(train_data)
 warnings.simplefilter('ignore')
@@ -124,23 +129,28 @@ cond_samples = model.sample_from_conditions(conditions)
 
 # Make the comparable distributions
 true_dist = make_dist(value_dict, truth)
+best_dist = make_dist(value_dict, best)
 tl_dist = make_dist(value_dict, source_dist)
 cond_dist = make_dist(value_dict, cond_samples)
 
 # Compare
-kl_div = np.zeros((len(compare_cols), 2))
-better, worse = [], []
+kl_div = np.zeros((len(compare_cols), 4))
 for idx, col in enumerate(compare_cols):
-    for subidx, (name, dist) in enumerate(zip(['TL Data', 'Cond Sample'], [tl_dist, cond_dist])):
-        kl_div[idx,subidx] = entropy(dist[col], true_dist[col])
+    for subidx, (name, a_dist, b_dist) in enumerate(zip(['TL vs Truth', 'TL vs Best', 'CS vs Truth', 'CS vs Best'],
+                                                     [tl_dist, tl_dist, cond_dist, cond_dist],
+                                                     [true_dist, best_dist, true_dist, best_dist])):
+        kl_div[idx,subidx] = entropy(a_dist[col], b_dist[col])
         if not np.isfinite(kl_div[idx,subidx]):
-            kl_div[idx,subidx] = entropy(true_dist[col], dist[col])
+            kl_div[idx,subidx] = entropy(b_dist[col], a_dist[col])
     td = np.asarray(true_dist[col])
+    bs = np.asarray(best_dist[col])
     tld = np.asarray(tl_dist[col])
     csd = np.asarray(cond_dist[col])
     warnings.simplefilter('ignore')
-    tld_arr = np.asarray([_ for _ in td * np.log(td / tld) if np.isfinite(_)])
-    csd_arr = np.asarray([_ for _ in td * np.log(td / csd) if np.isfinite(_)])
+    tld_truth_arr = np.asarray([_ for _ in td * np.log(td / tld) if np.isfinite(_)])
+    csd_truth_arr = np.asarray([_ for _ in td * np.log(td / csd) if np.isfinite(_)])
+    tld_best_arr = np.asarray([_ for _ in bs * np.log(bs / tld) if np.isfinite(_)])
+    csd_best_arr = np.asarray([_ for _ in bs * np.log(bs / csd) if np.isfinite(_)])
     warnings.simplefilter('default')
     if col in convert_cols:
         options = value_dict[col]
@@ -148,27 +158,23 @@ for idx, col in enumerate(compare_cols):
         options = value_dict[col][1]
     print(col, "KL Div:\t", kl_div[idx])
     print("Options:\t",options)
-    print("True Dist:\t", td, td * len(truth))
-    print("TL Data Dist:\t", tld, tld * len(source_dist))
-    print("Conditional Dist:\t", csd, csd * len(source_dist))
-    print("TL Partial KL:\t", tld_arr, tld_arr.sum())
-    print("CS Partial KL:\t", csd_arr, csd_arr.sum())
-    if kl_div[idx,0] - kl_div[idx,1] > 0:
-        better.append(col)
-        print("Conditional Sampling IMPROVES distribution")
-    else:
-        worse.append(col)
-        print("Conditional Sampling HARMS distribution")
+    print("True Dist:\t", td, (td * len(truth)).astype(int), sum((td * len(truth)).astype(int)))
+    print("Best Dist:\t", bs, (bs * len(best)).astype(int), sum((bs * len(best)).astype(int)))
+    print("TL Data Dist:\t", tld, (tld * len(source_dist)).astype(int), sum((tld * len(source_dist)).astype(int)))
+    print("Conditional Dist:\t", csd, (csd * len(cond_samples)).astype(int), sum((csd * len(cond_samples)).astype(int)))
+    print("TL vs Truth Partial KL:\t", tld_truth_arr, tld_truth_arr.sum())
+    print("CS vs Truth Partial KL:\t", csd_truth_arr, csd_truth_arr.sum())
+    print("TL vs Best Partial KL:\t", tld_best_arr, tld_best_arr.sum())
+    print("CS vs Best Partial KL:\t", csd_best_arr, csd_best_arr.sum())
     print()
     fig, ax = plt.subplots()
-    ax.plot(np.arange(len(options)), td, label="True Dist")
-    ax.plot(np.arange(len(options)), tld, label="TL Dist")
-    ax.plot(np.arange(len(options)), csd, label="CS Dist")
+    ax.plot(np.arange(len(options)), td, label="True Dist", marker='.')
+    ax.plot(np.arange(len(options)), bs, label="Best Dist", marker='+')
+    ax.plot(np.arange(len(options)), tld, label="TL Dist", linestyle='--', marker='o')
+    ax.plot(np.arange(len(options)), csd, label="CS Dist", linestyle='--', marker='*')
     ax.set_xticks(np.arange(len(options)))
     ax.set_xticklabels(options)
     ax.legend()
     ax.set_title(f"KL Divergence of {col}: {kl_div[idx]}")
-print("Improved:", better)
-print("Worsened:", worse)
 plt.show()
 
