33c0bc325dc9493e50f76ea01320df69ad509999
Tom Randall <tlranda@clemson.edu>
2023-08-25 18:29:46 +0000

    Update similarity check to use Gower similarity and plot

--- ytopt-libe-heffte/polaris/combine/similarity_check.py
+++ ytopt-libe-heffte/polaris/combine/similarity_check.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import pathlib
 import itertools
+import matplotlib.pyplot as plt
 
 from deinterpret import TopologyCache
 
@@ -30,7 +31,7 @@ for file in INSPECT:
     original_csv = pd.read_csv(file)
     original_csv['FLOPS'] *= -1
     # Quantiles based on successful evaluations only
-    csv = original_csv[original_csv['FLOPS'] > 0].sort_values(by='FLOPS')
+    csv = original_csv[original_csv['FLOPS'] > 0].sort_values(by=['FLOPS'])
     # Convert flexible parameters
     TPN = list(set(csv['threads_per_node']))[0]
     RPN = list(set(csv['ranks_per_node']))[0]
@@ -100,12 +101,76 @@ for file in INSPECT:
 
 del (topCache, top_keymap)
 
-import pdb
-pdb.set_trace()
+# Gower distance for similarity
+# Based on wwwjk366's Gower library (github.com/wwwjk366/gower)
+def gower(x,y, weights=None):
+    x_n_rows, x_n_cols = x.shape
+    y_n_rows, y_n_cols = y.shape
+    # Logical not of when X dtype for column is a numpy number type for all columns
+    cat_features = ~np.vectorize(np.issubdtype)(x.dtypes, np.number)
+    x, y = x.to_numpy(), y.to_numpy()
+    z = np.concatenate((x,y))
+    x_index = np.arange(0, x_n_rows)
+    y_index = np.arange(x_n_rows, x_n_rows+y_n_rows)
+
+    Z_numeric = z[:, np.logical_not(cat_features)].astype(np.float64)
+    Z_categor = z[:, cat_features]
+    num_cols = Z_numeric.shape[1]
+    num_ranges, num_max = np.zeros(num_cols), np.zeros(num_cols)
+
+    if weights is None:
+        weights = np.ones(z.shape[1])
+    weight_cat = weights[cat_features]
+    weight_num = weights[np.logical_not(cat_features)]
+    weight_sum = weights.sum()
+
+    # Normalize numeric data
+    for col in range(num_cols):
+        col_array = Z_numeric[:, col]
+        col_min, col_max = np.nanmin(col_array), np.nanmax(col_array)
+        if np.isnan(col_min):
+            col_min = 0.0
+        if np.isnan(col_max):
+            col_max = 0.0
+        num_max[col] = col_max
+        num_ranges[col] = np.abs(1 - col_min/col_max) if (col_max != 0) else 0.0
+    Z_numeric = np.divide(Z_numeric, num_max, out=np.zeros_like(Z_numeric), where=num_max != 0)
+
+    # Splitting by kinds
+    out = np.zeros((x_n_rows, y_n_rows), dtype=np.float64)
+    X_cat = Z_categor[x_index,]
+    X_num = Z_numeric[x_index,]
+    Y_cat = Z_categor[y_index,]
+    Y_num = Z_numeric[y_index,]
+
+    for i in range(x_n_rows):
+        j_start = 0 if x_n_rows != y_n_rows else i
+        # Categorical
+        sij_cat = np.where(X_cat[i,:] == Y_cat[j_start:y_n_rows,:], np.zeros_like(X_cat[i,:]), np.ones_like(X_cat[i,:]))
+        sum_cat = np.multiply(weight_cat, sij_cat).sum(axis=1)
+        # Numerical
+        abs_delta = np.absolute(X_num[i,:]-Y_num[j_start:y_n_rows,:])
+        sij_num = np.divide(abs_delta, num_ranges, out=np.zeros_like(abs_delta), where=num_ranges!=0)
+        sum_num = np.multiply(weight_num, sij_num).sum(axis=1)
+        # Combine
+        sums = np.divide(np.add(sum_cat, sum_num), weight_sum)
+        out[i,j_start:] = sums
+        if x_n_rows == y_n_rows:
+            out[i:, j_start] = sums
+    return out
+
+fig, ax = plt.subplots()
 for idx, (file, selected) in enumerate(zip(INSPECT, selections)):
-    #print(file)
     # Compare distances to BEST performing data
     best = selected.iloc[-1]
     best_vals = best[param_cols]
     # TODO: Weight columns by importance
+    best_frame = pd.DataFrame(best.values.reshape((1,-1)), columns=selected.columns).infer_objects()
+    similarities = gower(best_frame, selected)
+    if '1024a' in str(file):
+        line = ax.plot(np.arange(similarities.shape[1]), similarities[0,:], label=file.parent.stem)
+ax.set_ylabel("Gower Similarity to Known Optimum")
+ax.set_xlabel("FLOP/s ascending rank")
+ax.legend()
+plt.show()
 
