a8ebb5793a61d1fcf988a143e642878c01ceab8b
Tom Randall <tlranda@clemson.edu>
2023-09-11 20:04:50 +0000

    Reorganize job scripts based on interest

--- ytopt-libe-libpressio/job_scripts/interesting/sz_polaris.sh
+++ ytopt-libe-libpressio/job_scripts/interesting/sz_polaris.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 #PBS -l walltime=03:00:00
-#PBS -l select=10:system=polaris
+#PBS -l select=129:system=polaris
 #PBS -l filesystems=home:grand
 #PBS -A LibPressioTomo
 #PBS -q prod
@@ -8,12 +8,12 @@
 source /home/trandall/polaris_libpressio_env.sh;
 cd /home/trandall/ytune_23/tlranda-ytopt-libensemble/ytopt-libe-libpressio;
 
-jsons=( 'mgardgpu' ); # 'sz3'
-mpi_ranks=( 2 4 6 8 12 16 24 32 );
+jsons=( 'sz' ); # 'sz3'
+mpi_ranks=( 256 );
 #mpi_ranks=( 4 8 16 32 64 256 492 );
 #  nodes =   1 2  4  8 16  64 123
-workers=( 4 );
-# MAX nodes = 10 (2% cluster capacity)
+workers=( 2 );
+# MAX nodes = 64*2+1 = 129 (26% cluster capacity)
 # POLARIS has MAX 496 on PROD queue
 calls=0;
 for n_ranks in ${mpi_ranks[@]}; do
