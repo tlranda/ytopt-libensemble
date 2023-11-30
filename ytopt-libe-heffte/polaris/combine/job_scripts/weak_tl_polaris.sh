66e8eaaa7c7bf3532e9ca985e69cf46f60138f82
Tom Randall <tlranda@clemson.edu>
2023-11-30 17:27:10 +0000

    Fixing environment and job specification for TL

--- ytopt-libe-heffte/polaris/combine/job_scripts/weak_tl_polaris.sh
+++ ytopt-libe-heffte/polaris/combine/job_scripts/weak_tl_polaris.sh
@@ -5,11 +5,12 @@
 #PBS -A EE-ECP
 #PBS -q prod
 
-source /home/trandall/polaris_heffte_env.sh;
+source /home/trandall/polaris_knight_env.sh;
 cd /home/trandall/ytune_23/tlranda-ytopt-libensemble/ytopt-libe-heffte/polaris/combine;
 
 # App and MPI should scale together as weak-scalars
-app_scales=( 64 128 256 512 1024 1400 );
+#app_scales=( 64 128 256 512 1024 1400 );
+app3d_scales=( "0256_0256_0256" "0256_0256_0512" "0256_0512_0512" "0512_0512_0512" "0512_0512_1024" "0512_1024_1024" );
 mpi_ranks=(   8  16  32  64  128  256 );
 n_nodes=(     2   4   8  16   32   64 );
 n_workers=4;
@@ -20,25 +21,30 @@ calls=0;
 # Form the dataset to use as inputs
 dataset_basis="logs/PolarisSourceTasks/Polaris_"
 weak_dataset=( );
-for rank_index in ${!app_scales[@]}; do
-    str_app=${app_scales[$rank_index]};
-    while [[ ${#str_app} -lt 4 ]]; do
-        str_app="0${str_app}";
-    done;
+for rank_index in ${!app3d_scales[@]}; do
+    #str_app=${app_scales[$rank_index]};
+    #while [[ ${#str_app} -lt 4 ]]; do
+    #    str_app="0${str_app}";
+    #done;
+    str_app=${app3d_scales[$rank_index]};
     str_nodes=${n_nodes[$rank_index]};
     while [[ ${#str_nodes} -lt 2 ]]; do
         str_nodes="0${str_nodes}";
     done;
-    weak_dataset+=( "${dataset_basis}${str_nodes}n_${str_app}a_NoTopology/manager_results.csv" );
+    weak_dataset+=( "${dataset_basis}${str_nodes}n_${str_app}a/manager_results.csv" );
 done;
 
 for rank_idx in ${!weak_dataset[@]}; do
     left_out=${weak_dataset[$rank_idx]};
     n_ranks=${mpi_ranks[$rank_idx]};
     n_nodes=${n_nodes[$rank_idx]};
-    app_scale=${app_scales[$rank_idx]};
+    app_scale=${app3d_scales[$rank_idx]};
+    oIFS="$IFS";
+    IFS=_;
+    app_xyz=( ${app_scale} );
+    IFS="$oIFS";
     echo "Calling on ${n_workers} workers with ${n_ranks} mpi ranks (${n_nodes} nodes) per worker for size ${app_scale}";
-    call="python libEwrapper.py --mpi-ranks ${n_ranks} --worker-timeout 300 --application-scale ${app_scale} --gpu-enabled --ensemble-workers ${n_workers} --max-evals 30 --configure-environment craympi --machine-identifier polaris-gpu --system polaris --ens-dir-path Polaris_Weak_TL_${n_nodes}n_${app_scale}a --ens-template run_gctla.py --ens-script qsub_tl.batch --gc-input ${weak_dataset[@]} --gc-ignore ${left_out} --gc-initial-quantile 1.0 --launch-job --display-results";
+    call="python libEwrapper.py --mpi-ranks ${n_ranks} --worker-timeout 300 --application-x ${app_xyz[0]} --application-y ${app_xyz[1]} --application-z ${app_xyz[2]} --gpu-enabled --ensemble-workers ${n_workers} --max-evals 30 --configure-environment craympi --machine-identifier polaris-gpu --system polaris --ens-dir-path Polaris_Weak_TL_${n_nodes}n_${app_scale}a --ens-template run_gctla.py --ens-script qsub_tl.batch --gc-input ${weak_dataset[@]} --gc-ignore ${left_out} --gc-initial-quantile 1.0 --gc-app-x ${app_xyz[0]} --gc-app-y ${app_xyz[1]} --gc-app-z ${app_xyz[2]} --launch-job --display-results";
     date;
     echo "${call}";
     eval "${call}";
