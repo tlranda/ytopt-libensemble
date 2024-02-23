74cb66e78c526b99d85d0568cef7359ed0db98f9
Tom Randall <tlranda@clemson.edu>
2024-02-23 18:16:05 +0000

    Push it into the large queue

--- ytopt-libe-heffte/gctla/job_scripts/polaris_parallel_libe_script.sh
+++ ytopt-libe-heffte/gctla/job_scripts/polaris_parallel_libe_script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#PBS -l walltime=10:00:00
-#PBS -l select=65:system=polaris
+#PBS -l walltime=02:00:00
+#PBS -l select=257:system=polaris
 #PBS -l filesystems=home
 #PBS -A EE-ECP
 #PBS -q prod
 
 source /home/trandall/polaris_knight_env.sh;
 cd /home/trandall/ytune_23/tlranda-ytopt-libensemble/ytopt-libe-heffte/gctla;
-#python3 parallel_libe_driver.py --description weak_scaling_job.txt --max-nodes 64 --n-records 200 --sleep 1 --max-workers 4 --demo;
-python3 parallel_libe_driver.py --description weak_scaling_job.txt --max-nodes 64 --n-records 200 --sleep 60 --max-workers 4;
+#python3 parallel_libe_driver.py --description weak_scaling_job.txt --max-nodes 256 --n-records 200 --sleep 1 --max-workers 4 --demo;
+python3 parallel_libe_driver.py --description weak_scaling_job.txt --max-nodes 256 --n-records 200 --sleep 60 --max-workers 4;
 
