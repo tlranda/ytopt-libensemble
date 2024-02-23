22c0b3939b245e31582556ed4845cf7ba3a3f5d6
Tom Randall <tlranda@clemson.edu>
2024-02-23 17:22:09 +0000

    Qsub will log for us

--- ytopt-libe-heffte/gctla/job_scripts/polaris_parallel_libe_script.sh
+++ ytopt-libe-heffte/gctla/job_scripts/polaris_parallel_libe_script.sh
@@ -7,6 +7,6 @@
 
 source /home/trandall/polaris_knight_env.sh;
 cd /home/trandall/ytune_23/tlranda-ytopt-libensemble/ytopt-libe-heffte/gctla;
-#python3 parallel_libe_driver.py --description weak_scaling_job.txt --max-nodes 64 --n-records 200 --sleep 1 --max-workers 4 --demo > parallel_driver.output;
-python3 parallel_libe_driver.py --description weak_scaling_job.txt --max-nodes 64 --n-records 200 --sleep 60 --max-workers 4 > parallel_driver.output;
+#python3 parallel_libe_driver.py --description weak_scaling_job.txt --max-nodes 64 --n-records 200 --sleep 1 --max-workers 4 --demo;
+python3 parallel_libe_driver.py --description weak_scaling_job.txt --max-nodes 64 --n-records 200 --sleep 60 --max-workers 4;
 
