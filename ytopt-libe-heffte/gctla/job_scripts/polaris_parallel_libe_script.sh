f7b76e7fb196061b9b741e1972d37a92b0b6171b
Tom Randall <tlranda@clemson.edu>
2024-02-23 17:20:26 +0000

    Fix job script

--- ytopt-libe-heffte/gctla/job_scripts/polaris_parallel_libe_script.sh
+++ ytopt-libe-heffte/gctla/job_scripts/polaris_parallel_libe_script.sh
@@ -1,5 +1,12 @@
-#! /bin/bash
+#!/bin/bash
+#PBS -l walltime=10:00:00
+#PBS -l select=65:system=polaris
+#PBS -l filesystems=home
+#PBS -A EE-ECP
+#PBS -q prod
 
-source /home/trandall/polaris_knight_heffte_env.sh;
+source /home/trandall/polaris_knight_env.sh;
 cd /home/trandall/ytune_23/tlranda-ytopt-libensemble/ytopt-libe-heffte/gctla;
-python3 parallel_libe_driver.py --description weak_scaling_job_start.txt --max-nodes 2 --n-records 4 --sleep 1 --max-workers 1;
+#python3 parallel_libe_driver.py --description weak_scaling_job.txt --max-nodes 64 --n-records 200 --sleep 1 --max-workers 4 --demo > parallel_driver.output;
+python3 parallel_libe_driver.py --description weak_scaling_job.txt --max-nodes 64 --n-records 200 --sleep 60 --max-workers 4 > parallel_driver.output;
+
