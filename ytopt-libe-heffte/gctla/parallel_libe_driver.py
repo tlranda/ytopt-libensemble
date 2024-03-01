96a55eb0f44afb41120a6e7a2cea612d57c26565
Tom Randall <tlranda@clemson.edu>
2024-03-01 19:45:07 +0000

    Legibility updates for parallel_libe_driver
    NodeFileManager updates:
    * Can limit nodes to subset (and remove that limitation). Functionality is very limited for now
    
    PythonEval updates:
    * Make use of pretty_function_print() to make output legible while still indicating with sufficient specificity what function is being called
    
    JobInfo updates:
    * Exclude tasks from shortName() and make sure we explicitly call str() on values
    * Better output indentation to make it easier to scan outputs
    
    Argument updates:
    * Debug can be condensed for my sanity
    * Max nodes automatically detects nodefiles and limits you to that many nodes, but you can still use the argument if there isn't a nodefile or if you want to use a subset of your allocation
    * Rejected jobs are output at the end of job parsing so they are easier to identify
    * Verbose debugging will preview each job class's tasks
    
    Main updates:
    * More legibility updates for the printed outputs in main loop
    * Debug silences a lot more here

--- ytopt-libe-heffte/gctla/parallel_libe_driver.py
+++ ytopt-libe-heffte/gctla/parallel_libe_driver.py
@@ -18,6 +18,7 @@ import pandas as pd, numpy as np
 # its ensemble directory (ie: no cross-contamination) but this isn't ready yet.
 ###
 
+DEBUG_LEVEL=0
 
 ###
 # CLASSES
@@ -59,6 +60,37 @@ class NodeFileManager():
         self.n_nodes = self.node_list.shape[0]
         self.allocated = np.full((self.n_nodes), False, dtype=bool)
         self.tags = dict()
+        self.limited = False
+
+    def limit_nodes(self, n_nodes):
+        # Since this object does not control what happens to allocated nodes, reject any request
+        # that could lead to work returning without the ability to de-allocate its nodes
+        #
+        # In the future, this constraint can be relaxed by re-sorting the list to ensure all active
+        # allocations will return OK, but I am not implementing that yet as it should be unnecessary
+        if sum(self.allocated[n_nodes:]) > 0:
+            raise ValueError("Nodes are currently allocated beyond indicated limit's cutoff -- cannot fulfill request")
+        # Ensure the limitation can be removed later on
+        self.full_node_list = self.node_list
+        self.full_n_nodes = self.n_nodes
+        self.full_allocated = self.allocated
+        # Update values to limit
+        self.node_list = self.node_list.copy()[:n_nodes]
+        self.n_nodes = n_nodes
+        self.allocated = self.allocated.copy()[:n_nodes]
+        self.limited = True
+
+    def unlimit_nodes(self):
+        if not self.limited:
+            raise ValueError("Nodes are not currently limited")
+        self.node_list = self.full_node_list
+        self.n_nodes = self.full_n_nodes
+        currently_allocated = [idx for (idx, val) in self.allocated if val]
+        self.allocated = self.full_allocated
+        # Ensure state remains consistent, it may have changed since limits were put on
+        self.allocated[:] = False
+        self.allocated[currently_allocated] = True
+        self.limited = False
 
     def reserve_nodes(self, n_nodes, tag):
         if type(n_nodes) is str:
@@ -101,6 +133,18 @@ class NodeFileManager():
 # GLOBAL MANAGER FOR NODE LISTS
 NodeListMaker = NodeFileManager()
 
+def pretty_function_print(func):
+    if not callable(func):
+        return f"{func}"
+    if inspect.isbuiltin(func):
+        return f"builtin_function {func.__name__}"
+    if inspect.ismethod(func):
+        class_name = func.__self__.__class__.__name__
+        return f"class:{class_name} {func.__name__}"
+    if inspect.isfunction(func):
+        module_name = inspect.getmodule(func).__name__
+        return f"module:{module_name} {func.__name__}"
+
 def NoOp(*args,**kwargs):
     pass
 
@@ -113,7 +157,7 @@ class PythonEval():
         for (attr, value) in other_attrs.items():
             setattr(self, attr, value)
     def __str__(self):
-        return f"Calls {self.func} with:"+\
+        return f"Calls {pretty_function_print(self.func)} with:"+\
                 "\n\targs:\n\t\t"+"\n\t\t".join([str(_) for _ in self.args])+\
                 "\n\tkwargs:\n\t\t"+"\n\t\t".join([f"Key: '{k}'; Value: '{v}'" for (k,v) in self.kwargs.items()])
     def make_args(self, job):
@@ -130,6 +174,12 @@ class PythonEval():
         return new_args, new_kwargs
     def __call__(self, job):
         new_args, new_kwargs = self.make_args(job)
+        print(pretty_function_print(self.func))
+        if DEBUG_LEVEL > 0:
+            if len(new_args) > 0:
+                print("\targs:\n\t\t"+"\n\t\t".join([f"#{idx}: {pretty_function_print(arg)}" for idx, arg in enumerate(new_args)]))
+            if len(new_kwargs) > 0:
+                print("\tkwargs:\n\t\t"+"\n\t\t".join([f"Key: '{k}'; Value: '{pretty_function_print(v)}'" for (k,v) in new_kwargs.items()]))
         self.func(*new_args, **new_kwargs)
 
 # Format the shell command based on own attributes and track # of occupied nodes by the job
@@ -141,8 +191,12 @@ class JobInfo(SetWhenDefined):
     def __str__(self):
         return self.basic_job_string.format(**self.__dict__)
     def shortName(self):
-        except_spec = dict((k,v) for (k,v) in self.__dict__.items() if not k.startswith('_') and k not in ['spec'])
-        return str(except_spec)
+        other_attrs = dict()
+        for (k,v) in self.__dict__.items():
+            if k.startswith('_') or k in ['spec','prelaunch_tasks','postlaunch_tasks','finalize_tasks']:
+                continue
+            other_attrs[k] = str(v)
+        return str(self.__class__.__name__)+str(other_attrs)
     def specName(self):
         return self.spec
     def nodes(self):
@@ -150,17 +204,18 @@ class JobInfo(SetWhenDefined):
     def process_cmd_queue(self, queue, identifier):
         n_queued = len(queue)
         if n_queued == 0:
-            print(f"{identifier} -- Nothing to do!")
+            print("\t"+f"{identifier} -- Nothing to do!")
         for idx, cmd in enumerate(queue):
             if type(cmd) is str:
                 command_split = shlex.split(cmd.format(**self.__dict__))
-                print(f"{identifier} command {idx+1}/{n_queued}: {command_split}")
+                print("\t"+f"{identifier} command {idx+1}/{n_queued}: SHELL_COMMAND")
+                print("\t"+" ".join(command_split))
                 new_sub = subprocess.Popen(command_split)
                 retcode = new_sub.wait()
                 if retcode != 0:
                     raise ValueError("Command failed")
             elif isinstance(cmd, PythonEval):
-                print(f"{identifier} Python command {idx+1}/{n_queued}: {cmd}")
+                print("\t"+f"{identifier} Python command {idx+1}/{n_queued}: ", end="")
                 cmd(self)
     def prelaunch(self):
         self.process_cmd_queue(self.prelaunch_tasks, "Pre-launch")
@@ -257,21 +312,44 @@ def build():
     prs = argparse.ArgumentParser()
     prs.add_argument('--description', type=str, required=True, help="File with newline-delimited paths to forge or expand")
     prs.add_argument('--job-type', choices=['sleep','libE'], default='libE', help="Identify what kinds of jobs are described by --description (default: %(default)s)")
-    prs.add_argument('--max-nodes', type=int, required=True, help="Maximum nodes in parallel")
+    prs.add_argument('--max-nodes', type=int, default=None, help="Maximum nodes in parallel (required if PBS_NODEFILE is not set, shrinks to contents of PBS_NODEFILE if --max-nodes indicates more nodes than are listed there)")
     prs.add_argument('--n-records', type=int, required=True, help="Number of records required for each item in description list")
     prs.add_argument('--ranks-per-node', type=int, default=4, help="MPI rank expansion factor (default: %(default)s)")
     prs.add_argument('--max-workers', type=int, default=4, help="Max LibE workers (will be reduced for larger node jobs automatically; default: %(default)s)")
     prs.add_argument('--sleep', type=int, default=60, help="Sleep period (in seconds) before checking to start up more jobs (default: %(default)s)")
+    prs.add_argument('--debug-level', choices=['min','max'], default='min', help="Amount of debug output from this program (default: %(default)s)")
     prs.add_argument('--demo', action='store_true', help="Operate in demo mode (only echo jobs commands)")
     prs.add_argument('--pretend-polls', action='store_true', help="Demo mode pretends to poll jobs a small random number of times rather than just once per job (default: %(default)s)")
     return prs
 
 def parse(args=None, prs=None):
+    global DEBUG_LEVEL
     if prs is None:
         prs = build()
     if args is None:
         args = prs.parse_args()
+
+    # Not all code has direct access to args, just use this global value
+    if args.debug_level == 'max':
+        DEBUG_LEVEL = 1
+
+    # Max nodes may be set on the command line, but if PBS_NODEFILE is set in the environment,
+    # the node manager will only be capable of allocating those nodes. Therefore, --max-nodes
+    # can use any subset (including proper subset) of that many nodes, but cannot exceed it
+    if 'PBS_NODEFILE' in os.environ:
+        with open(os.environ["PBS_NODEFILE"], "r") as f:
+            nodefile_nodes = len(f.readlines())
+            print(f"PBS_NODEFILE, '{os.environ['PBS_NODEFILE']}' indicates {nodefile_nodes} nodes")
+        if args.max_nodes is None:
+            args.max_nodes = nodefile_nodes
+        elif args.max_nodes > nodefile_nodes:
+            print(f"REDUCING --max-nodes={args.max_nodes} to nodefile's limit of {nodefile_nodes}")
+            args.max_nodes = nodefile_nodes
+        elif args.max_nodes < nodefile_nodes:
+            NodeListMaker.limit_nodes(args.max_nodes)
+
     args.jobs = []
+    rejected_info = []
     with open(args.description, 'r') as spec:
         for line_idx, line in enumerate(spec.readlines()):
             line = line.lstrip().rstrip()
@@ -290,7 +368,8 @@ def parse(args=None, prs=None):
                 int_nodes = int(n_nodes)
                 # Reject if never runnable
                 if int_nodes > args.max_nodes:
-                    print(f"REJECT spec (Requested nodes {int_nodes} > Max nodes {args.max_nodes}) in {args.description}:{line_idx} \"{line}\"")
+                    # Add one so it lines up with 1-based line counting used by most editors
+                    rejected_info.append((int_nodes, line_idx+1, line))
                     continue
                 n_ranks = int_nodes * args.ranks_per_node
                 # Check if there are any existing results to extend
@@ -340,7 +419,23 @@ def parse(args=None, prs=None):
                                               libE_postlaunch_tasks,
                                               libE_finalize_tasks)
                     args.jobs.append(template_job)
-            print(f"Job description: {line} --> Job: {args.jobs[-1].shortName().rstrip()}")
+            if DEBUG_LEVEL > 0:
+                print(f"Job description: {line} --> Job: {args.jobs[-1].shortName().rstrip()}")
+    for (int_nodes, line_idx, line) in rejected_info:
+        print(f"!! --REJECT-- spec (Requested nodes {int_nodes} > Max nodes {args.max_nodes}) in {args.description}:{line_idx} \"{line}\"")
+    if DEBUG_LEVEL > 0:
+        described_classes = set()
+        for job in args.jobs:
+            jobclass = job.__class__.__name__
+            if jobclass not in described_classes:
+                described_classes.add(jobclass)
+                print(f"{jobclass} Jobs have the following responsibilities:")
+                if len(job.prelaunch_tasks) > 0:
+                    print("\tPre-Launch Tasks:",[pretty_function_print(_.func) if isinstance(_,PythonEval) else str(_) for _ in job.prelaunch_tasks])
+                if len(job.postlaunch_tasks) > 0:
+                    print("\tPost-Launch Tasks:",[pretty_function_print(_.func) if isinstance(_,PythonEval) else str(_) for _ in job.postlaunch_tasks])
+                if len(job.finalize_tasks) > 0:
+                    print("\tFinalize Tasks:",[pretty_function_print(_.func) if isinstance(_,PythonEval) else str(_) for _ in job.finalize_tasks])
     return args
 
 def execute_job(job_obj, args):
@@ -351,7 +446,7 @@ def execute_job(job_obj, args):
     command_split = job_obj.format()
     print(f"Launch job: {' '.join(command_split)}")
     if args.demo:
-        print("DEMO -- no job launch")
+        print("!! DEMO -- no job launch")
         new_sub = pretend_subprocess(command_split, None if args.pretend_polls else 1)
     else:
         fout = open("parallel_job.output","w")
@@ -383,18 +478,21 @@ def main(args=None):
         for idx, job in enumerate(args.jobs):
             n_nodes = job.nodes()
             if n_nodes + nodes_in_flight > args.max_nodes:
-                print(f"Job requests {n_nodes} nodes exceed max in flight -- {job.specName()}")
+                if DEBUG_LEVEL > 0:
+                    print(f"Job requests {n_nodes} nodes exceed max in flight -- {job.specName()}")
                 unused_queue.append(args.jobs[idx])
                 continue
             new_sub = execute_job(job, args)
             subprocess_queue[new_sub] = {'spec': job.specName(), 'nodes': job.nodes(), 'job': job}
             nodes_in_flight += n_nodes
         args.jobs = unused_queue
-        print("Running jobs:")
-        print("\t"+"\n\t".join([f"({spq_dict['nodes']} nodes): {spq_dict['spec']}" for spq_dict in subprocess_queue.values()]))
+        if DEBUG_LEVEL > 0:
+            print("Running jobs:")
+            print("\t"+"\n\t".join([f"({spq_dict['nodes']} nodes): {spq_dict['spec']}" for spq_dict in subprocess_queue.values()]))
         # VERY inactive poll between jobs -- maximize resources for everything else we don't need these to pop off immediately
         if len(subprocess_queue) > 0:
-            print(f"Sleep for {args.sleep} seconds")
+            if DEBUG_LEVEL > 0:
+                print(f"Sleep for {args.sleep} seconds")
             time.sleep(args.sleep)
     print("ALL jobs done")
 
